078e2647c817f4f40ebef82bb921560b3ca408ed
Ross Fenning <fennir01@mc-s083545.local>
2014-10-20 23:41:10 +0100

    Changed implementation to use requests and shelve.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name='testypie',
-      version='0.1.1',
+      version='0.2.0',
       author='Ross Fenning',
       author_email='ross.fenning@gmail.com',
       py_modules=['testypie'],
--- testypie.py
+++ testypie.py
@@ -1,12 +1,6 @@
-from flask import Flask, request, Response
-import os
-try:
-    from urllib.parse import quote
-except ImportError:
-    from urllib import quote
+import shelve
+from flask import Flask, request, Response, stream_with_context
 import requests
-import json
-import logging
 
 
 app = Flask(__name__)
@@ -18,26 +12,21 @@ BASEDIR = 'fixtures'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-    cache_file_name = os.path.join(BASEDIR, quote(request.url, ''))
+    with shelve.open(BASEDIR) as cache:
 
-    if not os.path.exists(BASEDIR):
-        os.makedirs(BASEDIR)
+        if request.url not in cache:
+            client_headers = {
+                'Accept': request.headers['Accept']
+            }
+            upstream = requests.get(request.url, allow_redirects=False, headers=client_headers)
 
-    if not os.path.exists(cache_file_name):
-        with open(cache_file_name, 'wb') as cache_file:
-            logging.info('No fixture for {}; downloading...', request.url)
-            upstream_response = requests.get(request.url, stream=True, allow_redirects=False)
-            cache_file.write(json.dumps(dict(upstream_response.headers)))
-            cache_file.write('\n')
-            cache_file.write(upstream_response.raw.read())
+            server_headers = {
+                'Content-Type': upstream.headers['content-type']
+            }
+            response = Response(response=upstream.content, status=upstream.status_code, headers=server_headers)
+            cache[request.url] = response
 
-    response = Response()
-
-    with open(cache_file_name, 'rb') as cache_file:
-        response.headers = json.loads(cache_file.readlines()[0].rstrip('\n'))
-
-    with open(cache_file_name, 'rb') as cache_file:
-        return ''.join(cache_file.readlines()[1:])
+        return cache[request.url]
 
 if __name__ == "__main__":
     app.run(debug=True)
